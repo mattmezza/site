a5eb24a5b54cc7d7ac2dfdd8ef3d3f5294fc11e4
Matteo Merola <mattmezza@gmail.com>
2017-08-06 01:10:32 +0200

    🎉 New article: remote working

---
+++ pages/2017/remote-working.js
@@ -0,0 +1,144 @@
+// Layouts
+import Post from '../../layouts/essay'
+
+// Components
+import P from '../../components/paragraph'
+import Meta from '../../components/meta'
+import Link from '../../components/link'
+import { Ref, Note, FootNotes } from '../../components/footnotes'
+
+export default () => (
+  <Post>
+    <Meta id="remote-working" hasCover />
+    <P>
+      When you think about work you usually think about an office, a workplace, people gathering together to work closely and produce something at the end of every day. But if you give a close look at what it is happening around you, you may realize that this imaginary is becoming a lil old.
+      Nowadays technology allows us to collaborate among each other in a very fast and convenient way. This enables the ability to shift from the classic way of managing work by interacting with people directly to a new digital way of working together.
+      More and more companies are switching to a liquid state, allowing their employees to start working from home, first gradually for some days per month, then for some days per week, then completely. This brings some benefits to not only employees but employers too. Alongside with the benefits, productivity and management threats are coming too, being able to takle these issues is a key concept to focus on when trying to adopt a liquid strategy.
+      <br />
+      In this post I am trying to give my point of view on remote working using my experience of remote vs. onsite engineer and manager by listing a few key points to pay attention to.
+    </P>
+
+    <h3>Allow employees to work remotely: keeping the best and brightest.</h3>
+    <P>
+      First of all, it is worth to mention that, especially if you don
+      {`'`}
+      t live in a metropolitan area, sometimes it can be really difficult to hire talents and to keep them with you in your company as the job market, especially today, is very dynamic. People in fact tend to move more often, workers they relocate cause of family or personal issues. Especially highly talented employees, they can quit easily cause they can find other job offers pretty much everywhere.
+      {' '}
+      By allowing your work force to work remotely you will have a broader audience to refer to. Your employees would be able to move, relocate or even live on a camper or in a trailer park
+      <Ref id="1" />
+    </P>
+
+    <h3>
+      Live outside the office and be free and productive with remote working.
+    </h3>
+    <P>
+      Allowing employees to work remotely is often equal to make their life easier. Having the possibility to organize your daily schedule as you wish gives the freedom to achieve more. Even workaholics can benefit from this option as they will get more free time to spend on side projects and non work life
+      <Ref id="2" />
+      . Imagine an employee has to see a doctor, or do some important document stuff, in an ordinary work life he would have had to whether take a day or half a day off from work for maybe a 30 minutes thingy. Being able to organize your time gives you the possibility to work more and better optimize time management.
+    </P>
+
+    <h3>Get rid of office distractions and improve quality of work done.</h3>
+    <P>
+      Getting to see your colleagues is really good and amazing but, sometimes, it is also very distracting. If you sum up all the time spent at the coffee machine or at the bar before going to the office you will find interesting results. Not that the minutes spent chatting at the coffee machine are useless. Sometimes they help a lot. The thing is, since your work does not solely depend on you and on your input, but it often is a product of many people
+      {`'`}
+      s minds, remote working helps to be focused on the main topic of the conversation. Playing and creating a binding among colleagues is both crucial and important, for this reason, internal communication and clarity is essential in these cases.
+    </P>
+
+    <h3>{`"`}Face time{`"`} doesn{`'`}t really equal productivity</h3>
+    <P>
+      Many managers evaluate the effort an employee spends on a project by taking into account the number of desk hours totally reached. If you are not in the office you are not productive in their vision. But the reality is that desk time does not equal productivity! Everyone is different, people have to be put in the best conditions to produce the best work. They gotta love what they do. Plus, today
+      {`'`}
+      s technology allows us to be always connected and always on, collaborating via video or screen sharing.
+      {' '}
+      <br />
+      Spend some time thinking about the recent meetings you attended and how they went, were they really needed? And most importantly, was it really worth to spend one entire hour when you compare the cost of time with the result of each one of those meetings?
+    </P>
+
+    <h3>
+      Give remote a try but take it slow to see whether it really works for your company.
+    </h3>
+    <P>
+      If you are considering to introduce remote working for your employees there are a few things you should care about. If you are starting to work remotely you should check the same things and try to understand whether the company you are starting to work for is implementing them or not as they will geatly impact on your work and, for this reason, on your life consequently.
+      <br />
+      First thing first: don
+      {`'`}
+      t think that managing employees who do not work from the same physical workplace is the same thing as the old onsite job concept. Working remotely requires some more attentions that must be given to how you organize work. Not only: you should encourage connections and give some more availability and flexibility to talk and to be reachable.
+      {' '}
+      <br />
+      As I was mentioning before, clarity plays a key factor role when organizing work. Information availability, clear goals for each project, well established procedures and crystal clear accountability of tasks and roles help giving an idea of the context in which a remote employee has work.
+      {' '}
+      <br />
+      As always, exceptions are here to remind us that we are not in control of 100% scenarios. There are some specific job positions that generally do not fit very well for remote. Nonetheless, you should start from the assumption that every job can be executed remotely. It is not that much infrequent to see even secretaries working remotely, taking care of almost everything from the other side of the city. There are companies completely specified in Virtual Assistence and that provide VAs to super busy executives.
+      <Ref id="3" />
+    </P>
+    <h3>
+      Make sure that effective collaboration is nurtured between employees and with clients.
+    </h3>
+    <P>
+      Remote employees should be encouraged to take action and to collaborate with their remote colleagues. First of all it creates a binding and it diffuse the company culture among the workforce. It also makes you feel like you are playing as a team and that your effort are reflected in the bigger picture of the final products that carry yours and your colleagues
+      {`'`}
+      {' '}
+      work. A virtual coffee machine room is often the right place where to break the ice and start to build some off work relationships.
+    </P>
+    <h3>Never forget there is a person behind that username.</h3>
+    <P>
+      Knowing that every employee has a different character and working style also affects the result of the whole team. Identifying whether a specific remote employee has a sociable, introverse, workaholic or leading attitude usually helps to assign tasks and set priorities.
+      {' '}
+    </P>
+    <h3>Avoid burnouts, some remote employees can work too much!</h3>
+    <P>
+      Especially at the beginning, some workers might end up working too much. Different factors can impact on this situation but the main reason why this effect happens is because remote workers don
+      {`'`}
+      t have time limits. They are not bound to office hours, they can be tempted to do that lil extra work everyday. Managers should realize this and act consequently by reducing the work load or giving extra day off. Eventually, employees
+      {`'`}
+      {' '}
+      happiness is crucial for not only productivity but also and especially for quality.
+      <br />
+      Another thing you should really care about is to always ask for remote worker
+      {`'`}
+      s needs. In fact, because remote employees do not live the office dynamics, they can start feeling like part of an inferior class of employees. This can lead to productivity issues and workers burnouts.
+    </P>
+    <h3>Be sure that managers are competent in remote work</h3>
+    <P>
+      Once again, if you are new to remote working and willing to start, to have to keep in mind that managing remote workers is different from the classic office way of organizing work and managing time. Your processes and procedures must be adapted for remote work and managers must be really competent on this topic in order to get the maximum results from this new way of working. Prefer managers with remote work experience as they already have an idea of how it can work and what the main problems might be.
+    </P>
+    <h3>Conclusions</h3>
+    <P>
+      Nowadays technology offers an incredible opportunity to everyone of us. We can benefit from remote work in a sustainable way, helping first of all ourselves and our happiness, then our company and eventually the entire society
+      <Ref id="4" />
+      . However, working remotely creates dynamics that can be very different from office work. This difference forces us to rethink the way we manage work in order to adapt the current workflow to get happier and more productive and happier workers.
+    </P>
+
+    <FootNotes>
+      <Note id="1">
+        As long as they will have a reliable Internet connection 😆
+      </Note>
+      <Note id="2">If they have one 😆</Note>
+      <Note id="3">
+        See
+        {' '}
+        <Link href="https://www.getleverage.com">leverage.com</Link>
+        {' '}
+        (I suggest to read the story of how
+        {' '}
+        <em>Ari Meisel</em>
+        {' '}
+        and
+        {' '}
+        <em>Nick Sonnenberg</em>
+        {' '}
+        founded
+        {' '}
+        <em>{'"Less Doing"'}</em>
+        {' '}
+        in
+        {' '}
+        <Link href="https://www.amazon.com/Idea-Execution-Optimize-Outsource-Everything/dp/1619615053">{`"Idea to Execution"`}</Link>
+        )
+      </Note>
+      <Note id="4">
+        Think about all the fuel and emission you save by not having to physically move from your place to the office, scale it big and get the result of how good it would be for the planet.
+      </Note>
+    </FootNotes>
+  </Post>
+)
