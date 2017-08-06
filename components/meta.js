f93d47eea51af2db55d28969ffa211cc26c75438
Matteo Merola <mattmezza@gmail.com>
2017-08-07 00:03:18 +0200

    🐛 Facebook article pic

--- components/meta.js
+++ components/meta.js
@@ -32,7 +32,7 @@ export default ({ id, hasCover }) => {
       <Back to="/thoughts" outsideCover={hasCover} />
 
       {hasCover && <Image src={coverURL} isCover />}
-      {hasCover && <link rel="image_src" href={coverURL} />}
+      {hasCover && <Head><link rel="image_src" href={coverURL} /></Head>}
       <Title value={post.title} />
 
       <h1 className={headingClass}>{post.title}</h1>
